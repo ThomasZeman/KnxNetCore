1bbb33eb1252daa10f6efe12cea12b2be387c44d
Thomas Zeman <git@thomas-zeman.com>
2017-04-17 20:26:10 +0200

    Added IndividualAddress
    Added IndividualAddress

---
+++ KnxNetCore.UnitTests/Telegrams/IndividualAddressTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using KnxNetCore.Telegrams;
+using Xunit;
+
+namespace KnxNetCore.UnitTests.Telegrams
+{
+    public class IndividualAddressTests
+    {
+        [Fact]
+        public void Address_0_1_2_is_ushort_address_258()
+        {
+            var sut = new IndividualAddress(0, 1, 2);
+            Assert.Equal(258, sut.AsUShort);
+        }
+
+        [Fact]
+        public void UShort_address_258_is_address_0_1_2()
+        {
+            var sut = new IndividualAddress(258);
+            Assert.Equal(new byte[] { 0, 1, 2 }, sut.As3Level);
+        }
+
+        [Fact]
+        public void Address_7_14_212_is_ushort_address_32468()
+        {
+            var sut = new IndividualAddress(7, 14, 212);
+            Assert.Equal(32468, sut.AsUShort);
+        }
+
+        [Fact]
+        public void UShort_address_32468_is_address_7_14_212()
+        {
+            var sut = new IndividualAddress(32468);
+            Assert.Equal(new byte[] { 7, 14, 212 }, sut.As3Level);
+        }
+
+        [Fact]
+        public void Address_with_area_18_cannot_be_constructed()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new IndividualAddress(18, 0, 0));
+        }
+
+        [Fact]
+        public void Address_with_line_19_cannot_be_constructed()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new IndividualAddress(0, 19, 0));
+        }
+    }
+}
---
+++ KnxNetCore/Telegrams/IndividualAddress.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace KnxNetCore.Telegrams
+{
+    public sealed class IndividualAddress
+    {
+        public static readonly int AreaBitWidth = 4;
+        public static readonly int LineBitWidth = 4;
+
+        public static readonly int AreaMax = (1 << AreaBitWidth) - 1;
+        public static readonly int LineMax = (1 << LineBitWidth) - 1;
+
+        private static readonly int AreaBitShift = 12;
+        private static readonly int LineBitShift = 8;
+
+        public ushort AsUShort { get; }
+
+        public IndividualAddress(ushort asUShort)
+        {
+            AsUShort = asUShort;
+        }
+
+        public IndividualAddress(byte area, byte line, byte device)
+        {
+            if (area > AreaMax)
+            {
+                throw new ArgumentOutOfRangeException($"Area cannot be greater than {AreaMax} but was {area}");
+            }
+            if (line > LineMax)
+            {
+                throw new ArgumentOutOfRangeException($"Line cannot be greater than {LineMax} but was {line}");
+            }
+            AsUShort = (ushort)(device + (line << LineBitShift) + (area << AreaBitShift));
+        }
+
+        public byte[] As3Level => new[]
+        {
+            (byte) (AsUShort >> AreaBitShift),
+            (byte) ((AsUShort >> LineBitShift) & LineMax),
+            (byte) (AsUShort)
+        };
+    }
+}
