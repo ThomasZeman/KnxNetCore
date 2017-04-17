fc0c32775746c641b72f2c52edebc87a51f03124
Thomas Zeman <git@thomas-zeman.com>
2017-04-17 19:14:36 +0200

    Added GroupAddress class
    Added GroupAddress class

---
+++ KnxNetCore.UnitTests/Telegrams/GroupAddressTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KnxNetCore.Telegrams;
+using Xunit;
+
+namespace KnxNetCore.UnitTests.Telegrams
+{
+    public class GroupAddressTests
+    {
+        [Fact]
+        public void UShort_address_775_is_0_3_7()
+        {
+            var sut = new GroupAddress(775);
+            var result = sut.As3Level;
+            Assert.Equal(new byte[] { 0, 3, 7 }, result);
+        }
+
+        [Fact]
+        public void Address_0_3_7_is_ushort_address_775()
+        {
+            var sut = new GroupAddress(0, 3, 7);
+            Assert.Equal(sut.AsUShort, 775);
+        }
+
+        [Fact]
+        public void UShort_address_40577_is_12_6_129()
+        {
+            var sut = new GroupAddress(40577);
+            var result = sut.As3Level;
+            Assert.Equal(new byte[] { 19, 6, 129 }, result);
+        }
+
+        [Fact]
+        public void Address_12_6_129_is_ushort_address_40577()
+        {
+            var sut = new GroupAddress(19, 6, 129);
+            Assert.Equal(sut.AsUShort, 40577);
+        }
+
+        [Fact]
+        public void Address_with_main_group_33_cannot_be_constructed()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new GroupAddress(33, 0, 0));
+        }
+
+        [Fact]
+        public void Address_with_middle_group_10_cannot_be_constructed()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new GroupAddress(0, 10, 0));
+        }
+    }
+}
---
+++ KnxNetCore/Telegrams/GroupAddress.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KnxNetCore.Telegrams
+{
+    /// <summary>
+    /// Represents a 3-level group address
+    /// </summary>
+    /// <remarks>2-level addresses or free address structure are not supported</remarks>
+    public class GroupAddress
+    {
+        public static readonly byte MainGroupMax = 31;
+        public static readonly byte MiddleGroupMax = 7;
+
+        public GroupAddress(ushort groupAddressAsUShort)
+        {
+            AsUShort = groupAddressAsUShort;
+        }
+
+        public GroupAddress(byte mainGroup, byte middleGroup, byte subGroup)
+        {
+            if (mainGroup > MainGroupMax)
+            {
+                throw new ArgumentOutOfRangeException($"MainGroup cannot be greater than {MainGroupMax} but was {mainGroup}");
+            }
+            if (middleGroup > MiddleGroupMax)
+            {
+                throw new ArgumentOutOfRangeException($"MiddleGroup cannot be greater than {MiddleGroupMax} but was {middleGroup}");
+            }
+            AsUShort = (ushort)(subGroup + (middleGroup << 8) + (mainGroup << 11));
+        }
+
+        public ushort AsUShort { get; }
+
+        public byte[] As3Level => new byte[]
+        {
+            (byte) (AsUShort >> 11),
+            (byte) ((AsUShort >> 8) & 7),
+            (byte) AsUShort
+        };
+    }
+}
