e0dc0b7050cb51be1ece145859adff50e16fa93d
Thomas Zeman <git@thomas-zeman.com>
2017-04-18 23:49:21 +0200

    Added Dpt9 float bytes and bytes float conversion

---
+++ KnxNetCore.UnitTests/Datapoints/Dpt9Tests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using KnxNetCore.Datapoints;
+using Xunit;
+
+namespace KnxNetCore.UnitTests.Datapoints
+{
+    public class Dpt9Tests
+    {
+        // Input/Output data has been verified with ETS5
+        public static IEnumerable<object[]> TestData => new[]
+        {
+            new object[] {new byte[] {0x0c, 0x79}, 22.9d},
+            new object[] {new byte[] {0x0c, 0x42}, 21.8d},
+            new object[] {new byte[] {0x0c, 0x15}, 20.9d},
+            new object[] {new byte[] {0x47, 0xa1}, 4999.68d},
+            new object[] {new byte[] {0x64, 0xc4}, 49971.2d},
+            new object[] {new byte[] {0x64, 0xc4}, 49971.2d},
+            new object[] {new byte[] {0x7f, 0xff}, 670760.96d},
+            new object[] {new byte[] {0xf8, 0x02}, -670433.28d},
+            new object[] {new byte[] {0xf8, 0x01}, -670760.96d},
+            new object[] {new byte[] {0x8a, 0xc5}, -26.78d},
+            new object[] {new byte[] {0xa1, 0x56}, -272.96d},
+            new object[] {new byte[] {0x00, 0x00}, 0d},
+            new object[] {new byte[] {0x00, 0x01}, 0.01d},
+            new object[] {new byte[] {0x87, 0xff}, -0.01d},
+            new object[] {new byte[] {0x00, 0x64}, 1d},
+            new object[] {new byte[] {0x87, 0x9c}, -1d}
+        };
+
+        [Theory, MemberData(nameof(TestData))]
+        public void BytesToDouble(byte[] input, double expected)
+        {
+            var result = Dpt9.BytesToDouble(input);
+            Assert.Equal(expected, result, 3);
+        }
+
+        [Theory, MemberData(nameof(TestData))]
+        public void DoubleToBytes(byte[] input, double expected)
+        {
+            byte[] outBytes = new byte[5];
+            Dpt9.DoubleToBytes(expected, outBytes, 3);
+            Assert.Equal(input, outBytes.Skip(3).ToArray());
+        }
+    }
+}
---
+++ KnxNetCore/Datapoints/Dpt9.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace KnxNetCore.Datapoints
+{
+    public static class Dpt9
+    {
+        // based on: http://www.knx.org/fileadmin/template/documents/downloads_support_menu/KNX_tutor_seminar_page/Advanced_documentation/05_Interworking_E1209.pdf
+        // chapter 4.10
+
+        public static double BytesToDouble(byte[] input)
+        {
+            var mantissa = (input[1] | (input[0] & 7) << 8);
+            var exponent = (input[0] >> 3) & 15;
+            var calculatedExponent = Math.Pow(2, exponent);
+            if ((input[0] & 128) != 128)
+            {
+                // positive value
+                return Math.Round(0.01d * mantissa * calculatedExponent, 2);
+            }
+            // Remember: 1 = 0000 0000 0000 0001
+            //          -1 = 1111 1111 1111 1111
+            //
+            // Binary counting is represented in decimal system as [0..32767..-32768..-1]
+
+            // The reverse operation of the description in the knx documentation
+            // Substract 1 from mantissa, negate, cut down to 11bit and treat it as negative value
+            var mantissaMinusOne = (mantissa - 1);
+            var negated = ~mantissaMinusOne;
+            var cutTo11Bit = negated & 0x07ff;
+            return Math.Round(0.01d * -cutTo11Bit * calculatedExponent, 2);
+        }
+
+        public static void DoubleToBytes(double input, byte[] outputBytes, int offset)
+        {
+            if (outputBytes.Length - offset < 2)
+            {
+                throw new ArgumentException($"{outputBytes} has length {outputBytes.Length} and does not provide enough space with offset {offset}");
+            }
+            int exponent = 0;
+            var scaled = Math.Round(input * 100d);
+            while (scaled > 2047)
+            {
+                exponent++;
+                scaled /= 2d;
+            }
+            while (scaled < -2048)
+            {
+                exponent++;
+                scaled /= 2d;
+            }
+            if (exponent > 15)
+            {
+                throw new ArgumentOutOfRangeException($"Cannot convert {input} because it would have an exponent of {exponent} which is not supported");
+            }
+            int mantiassa;
+            if (scaled >= 0)
+            {
+                mantiassa = (int)scaled;
+                outputBytes[0] = 0;
+            }
+            else
+            {
+                mantiassa = (int)-scaled;
+                mantiassa = ~mantiassa;
+                mantiassa++;
+                outputBytes[offset] = 0x80;
+            }
+            outputBytes[offset] = (byte)(outputBytes[offset] | (exponent << 3) | (mantiassa >> 8) & 7);
+            outputBytes[offset + 1] = (byte)mantiassa;
+        }
+    }
+}
