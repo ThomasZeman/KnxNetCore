aa07a35e3453e9b077ca13d4e940a5b025e2f8b4
Thomas Zeman <git@thomas-zeman.com>
2017-04-27 21:45:53 +0200

    Added Amplify as NuGet dep

---
+++ KnxNetCore/Celsius.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Amplifier.Units;
+
+namespace KnxNetCore
+{
+    public class Celsius : IUnit<Celsius>
+    {
+        public Measure<Celsius> Convert(IMeasure source)
+        {
+            // Remove this when Amplify is updated
+            throw new NotImplementedException();
+        }
+    }
+}
