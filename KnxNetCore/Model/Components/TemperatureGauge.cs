5e667e52d9ae54139c4919f3451c71e4ab039dad
Thomas Zeman <git@thomas-zeman.com>
2017-05-12 23:15:22 +0200

    Provide exp temperature Get operation

--- KnxNetCore/Model/Components/TemperatureGauge.cs
+++ KnxNetCore/Model/Components/TemperatureGauge.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
+using Amplifier.Units;
+using KnxNetCore;
 using KnxRadio.Messages;
 
 namespace KnxRadio.Model.Components
 {
     public class TemperatureGauge : IComponent
     {
+        private readonly List<Measure<Celsius>> _temperatures = new List<Measure<Celsius>>();
+
+        public List<Measure<Celsius>> Temperatures => _temperatures;
+
         public void AddedToEntity(Entity entity)
         {
         }
@@ -18,6 +25,7 @@ namespace KnxRadio.Model.Components
             if (temperatureMessage != null)
             {
                 Console.WriteLine("TemperatureGauge: {0}", temperatureMessage.Temperature);
+                _temperatures.Add(temperatureMessage.Temperature);
             }
             return Task.FromResult(true);
         }
--- KnxRadio/Program.cs
+++ KnxRadio/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Amplifier.Units;
 using KnxNetCore;
 using KnxNetCore.Telegrams;
+using KnxRadio.Messages;
 using KnxRadio.Model.Components;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -31,15 +36,32 @@ namespace KnxRadio
         #region snippet_Configure
         public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
         {
+            var test = (ITest)app.ApplicationServices.GetService(typeof(ITest));
+            //var test = app.ServerFeatures.Get<ITest>();
             var serverAddressesFeature = app.ServerFeatures.Get<IServerAddressesFeature>();
             var routerBuilder = new RouteBuilder(app, null);
             routerBuilder.MapGet("temp/{id}", context =>
             {
-                return context.Response.WriteAsync("id:" + context.GetRouteData().Values["id"]);
+                return context.Response.WriteAsync("Temperatures: " + string.Join(Environment.NewLine, test.Messages.Select(_ => _.ToString())));
             });
             app.UseRouter(routerBuilder.Build());
         }
         #endregion
+    }
+
+    public interface ITest
+    {
+        List<Measure<Celsius>> Messages { get; }
+    }
+
+    class Test : ITest
+    {
+        public List<Measure<Celsius>> Messages { get; }
+
+        public Test(List<Measure<Celsius>> messages)
+        {
+            Messages = messages;
+        }
     }
 
     public class Program
@@ -47,13 +69,13 @@ namespace KnxRadio
 
         private static ITrace _trace;
 
-        public static IDisposable RunAsp()
+        public static IDisposable RunAsp(ITest test)
         {
             var builder = new WebHostBuilder()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .ConfigureServices(ConfigureServices)
+                .ConfigureServices(collection => ConfigureServices(collection, test))
                 .UseConfiguration(new ConfigurationBuilder().Build())
-                .UseStartup<Startup>()                
+                .UseStartup<Startup>()
                 .UseKestrel(options =>
                 {
                     options.ThreadCount = 1;
@@ -65,39 +87,42 @@ namespace KnxRadio
             return host;
         }
 
-        private static void ConfigureServices(IServiceCollection obj)
+        private static void ConfigureServices(IServiceCollection obj, ITest test)
         {
-           obj.AddRouting();
+            obj.AddRouting();
+            obj.AddSingleton<ITest>(test);
         }
 
         public static void Main(string[] args)
         {
-            using (RunAsp())
+            // Establish tunneling connection with gateway at 10.0.0.135
+            _trace = new ConsoleTrace();
+            var connection = new KnxConnection(new IPEndPoint(IPAddress.Parse("10.0.0.102"), 50000), new IPEndPoint(IPAddress.Parse("10.0.0.135"), 3671), _trace);
+
+
+            var messageBus = new MessageBus();
+
+            var dressingLight = new Entity(messageBus,
+                new IntegerMessageBusAddress(123),
+                new[] { new Switch() });
+
+            var button = new Button(new IntegerMessageBusAddress(123));
+            var dressingLightButton = new Entity(messageBus, new IntegerMessageBusAddress(456), new[] { button });
+
+            var temperatureGauge = new TemperatureGauge();
+            var temperatureLivingRoom = new Entity(messageBus, new IntegerMessageBusAddress(1001), new[] { temperatureGauge });
+
+            var radio = new Entity(messageBus, new IntegerMessageBusAddress(999), new[] { new Radio() });
+
+            var binding = new KnxBinding(connection, messageBus, new IntegerMessageBusAddress(10000));
+            binding.AddSwitch(GroupAddress.FromGroups(0, 0, 6), new IntegerMessageBusAddress(123), KnxAddressBindingTypes.Switch);
+            binding.AddSwitch(GroupAddress.FromGroups(0, 4, 0), new IntegerMessageBusAddress(999), KnxAddressBindingTypes.Switch);
+            binding.AddSwitch(GroupAddress.FromGroups(0, 3, 20), new IntegerMessageBusAddress(1001), KnxAddressBindingTypes.Temperature);
+            binding.AddSwitch(GroupAddress.FromGroups(0, 3, 21), new IntegerMessageBusAddress(1001), KnxAddressBindingTypes.Temperature);
+
+            var test = new Test(temperatureGauge.Temperatures);
+            using (RunAsp(test))
             {
-
-                // Establish tunneling connection with gateway at 10.0.0.135
-                _trace = new ConsoleTrace();
-                var connection = new KnxConnection(new IPEndPoint(IPAddress.Parse("10.0.0.102"), 50000), new IPEndPoint(IPAddress.Parse("10.0.0.135"), 3671), _trace);
-
-
-                var messageBus = new MessageBus();
-
-                var dressingLight = new Entity(messageBus,
-                    new IntegerMessageBusAddress(123),
-                    new[] { new Switch() });
-
-                var button = new Button(new IntegerMessageBusAddress(123));
-                var dressingLightButton = new Entity(messageBus, new IntegerMessageBusAddress(456), new[] { button });
-
-                var temperatureLivingRoom = new Entity(messageBus, new IntegerMessageBusAddress(1001), new[] { new TemperatureGauge() });
-
-                var radio = new Entity(messageBus, new IntegerMessageBusAddress(999), new[] { new Radio() });
-
-                var binding = new KnxBinding(connection, messageBus, new IntegerMessageBusAddress(10000));
-                binding.AddSwitch(GroupAddress.FromGroups(0, 0, 6), new IntegerMessageBusAddress(123), KnxAddressBindingTypes.Switch);
-                binding.AddSwitch(GroupAddress.FromGroups(0, 4, 0), new IntegerMessageBusAddress(999), KnxAddressBindingTypes.Switch);
-                binding.AddSwitch(GroupAddress.FromGroups(0, 3, 20), new IntegerMessageBusAddress(1001), KnxAddressBindingTypes.Temperature);
-                binding.AddSwitch(GroupAddress.FromGroups(0, 3, 21), new IntegerMessageBusAddress(1001), KnxAddressBindingTypes.Temperature);
                 for (;;)
                 {
                     var ck = Console.ReadKey();
